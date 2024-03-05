ed9dd079498ce2cf554ef5b4c79736927e41af6d
mdnazmulkarim <mdnazmulkarim@u.boisestate.edu>
2024-03-05 14:32:17 -0700

    add console log

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ const App = () => {
             const response = await fetch(`${API_URL}&s=${title}`);
             const data = await response.json();
 
+            console.log(data.Search);
+
             setMovies(data.Search);
     }
 
